37d98e69a7339829410101e170b94fad2a49f64c
Paal RINGSTAD <pringsta@e2r1p13.42.fr>
2017-06-03 19:40:02 +0200

    ..

--- main.c
+++ main.c
@@ -6,18 +6,38 @@
 /*   By: mtrazzi <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/03 17:28:40 by mtrazzi           #+#    #+#             */
-/*   Updated: 2017/06/03 17:32:33 by mtrazzi          ###   ########.fr       */
+/*   Updated: 2017/06/03 19:38:45 by pringsta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
+#include <stdio.h> 
+
+char	*ft_return_tet(char *av1)
+{	
+	int		fd;
+	int		ret;
+	int		len;
+	char	*buff;
+
+	len = 0;
+	fd = open(av1, O_RDONLY);
+	buff = ft_strnew(20);
+	while ((ret = read(fd, buff, 20)) > 0)
+		len += ret;
+	close (fd);
+	buff = ft_strnew(len);
+	fd = open(av1, O_RDONLY);
+	ret = read(fd, buff, len);
+	return (buff);
+}
 
 int		main(int ac, char **av)
 {
-	int fd;
-
-	fd = open(av[1], O_RDONLY);
+	char	*buff;
 	if (ac != 2)
 		ft_putendl_fd("error", 2);
+	buff = ft_return_tet(av[1]);
+	printf("II -> \n%s\n", buff);
 	return (0);
 }
