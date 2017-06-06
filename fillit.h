f362837a4ab575444fac14e9cd9c48e215a47548
Paal RINGSTAD <pringsta@e1r10p9.42.fr>
2017-06-06 16:33:57 +0200

    commiting before merging branch

--- fillit.h
+++ fillit.h
@@ -7,6 +7,7 @@
 int		diff_tetri(char *s, char *t, size_t n, char c);
 int		is_str_square(char *str);
 int		is_input_well_formated(char *str);
+int		ft_corr_input(char *input);
 char	*tetris_type(char *pointer, char c);
 char	*ft_read(char *av1);
 
--- main.c
+++ main.c
@@ -5,9 +5,16 @@ int		main(int ac, char **av)
 {
 	char	*buff;
 	if (ac != 2)
-		ft_putendl_fd("error", 2);
+	{
+		ft_putstr_fd("usage: fillit tetriminos_input_file", 2);
+		return (0);
+	}
 	buff = ft_read(av[1]);
-	printf("%s\n", buff);
-	ft_putendl(buff);
+	if (!ft_corr_input(buff))
+	{	
+		ft_putendl_fd("error", 2);
+		return (0);
+	}
+	ft_putendl("INPUT CORRECT YESSSSS");
 	return (0);
 }
