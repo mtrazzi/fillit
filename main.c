3a776207130ea71af9978e3f7893a9880e6bc431
mtrazzi <mtrazzi@student.42.fr>
2017-06-06 19:23:48 +0200

    corrected most important bugs

--- main.c
+++ main.c
@@ -15,16 +15,10 @@ int		main(int ac, char **av)
 	}
 	buff = ft_read(av[1]);
 	if (!ft_corr_input(buff))
-	{	
-		ft_putendl_fd("usage : fillit sample.fillit", 2);
-		return (0);
-	}
-	if (!ft_corr_input(av[1]))
 	{
-		ft_putendl("error");
+		ft_putendl_fd("error", 1);
 		return (0);
 	}
-	buff = ft_read(av[1]);
 	n = ft_sqrt(6 * ((ft_strlen(buff) + 1) / 21));
 	while (!ft_count(buff, ft_empty_grid(n), 0, 'A'))
 		n++;
